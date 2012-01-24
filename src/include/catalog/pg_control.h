94475ffc0e1ba1e32e5cdc0c07cb41b8effb3812
Alvaro Herrera <alvherre@alvh.no-ip.org>
2012-01-24 12:01:36 -0300

    bump pg_control

--- src/include/catalog/pg_control.h
+++ src/include/catalog/pg_control.h
@@ -21,7 +21,7 @@
 
 
 /* Version identifier for this pg_control format */
-#define PG_CONTROL_VERSION	921
+#define PG_CONTROL_VERSION	922
 
 /*
  * Body of CheckPoint XLOG records.  This is declared here because we keep
