298255242243bc42266356bf95af3f7f681cd6da
Alvaro Herrera <alvherre@alvh.no-ip.org>
2024-01-24 12:10:27 +0100

    fix s_lock_test compile

--- src/backend/storage/lmgr/s_lock.c
+++ src/backend/storage/lmgr/s_lock.c
@@ -61,6 +61,10 @@
 #define MIN_DELAY_USEC		1000L
 #define MAX_DELAY_USEC		1000000L
 
+#ifdef S_LOCK_TEST
+static uint32 local_my_wait_event_info;
+uint32 *my_wait_event_info = &local_my_wait_event_info;
+#endif
 
 slock_t		dummy_spinlock;
 
