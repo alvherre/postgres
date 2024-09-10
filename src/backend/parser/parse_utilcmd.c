ee15ab8a88fe0214d94f6d4d71b51830021783f2
Álvaro Herrera <alvherre@alvh.no-ip.org>
2024-09-10 19:10:05 +0200

    Don't call a function as list argument to foreach

--- src/backend/parser/parse_utilcmd.c
+++ src/backend/parser/parse_utilcmd.c
@@ -1081,6 +1081,7 @@ transformTableLikeClause(CreateStmtContext *cxt, TableLikeClause *table_like_cla
 	AclResult	aclresult;
 	char	   *comment;
 	ParseCallbackState pcbstate;
+	List	   *lst;
 
 	setup_parser_errposition_callback(&pcbstate, cxt->pstate,
 									  table_like_clause->relation->location);
@@ -1225,9 +1226,8 @@ transformTableLikeClause(CreateStmtContext *cxt, TableLikeClause *table_like_cla
 	 * Reproduce not-null constraints by copying them.  This doesn't require
 	 * any option to have been given.
 	 */
-	cxt->nnconstraints = list_concat(cxt->nnconstraints,
-									 RelationGetNotNullConstraints(RelationGetRelid(relation),
-																   false));
+	lst = RelationGetNotNullConstraints(RelationGetRelid(relation), false);
+	cxt->nnconstraints = list_concat(cxt->nnconstraints, lst);
 
 	/*
 	 * We cannot yet deal with defaults, CHECK constraints, indexes, or
